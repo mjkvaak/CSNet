3185a0ecfd52d22920ca85a76b915ac6563ae614
mjkvaak <37109302+mjkvaak@users.noreply.github.com>
2020-01-04 13:24:04 +0200

    Update drive.py

--- dataloader/drive.py
+++ dataloader/drive.py
@@ -94,7 +94,7 @@ class Data(Dataset):
         img_path = self.images[idx]
         gt_path = self.groundtruth[idx]
         image = Image.open(img_path)
-        label = Image.open(gt_path)
+        label = Image.open(gt_path).convert('LA')
 
         image = self.rescale(image, self.resize)
         label = self.rescale(label, self.resize)
